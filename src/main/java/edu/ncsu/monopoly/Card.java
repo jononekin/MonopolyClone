14f2e5837a5018a987f78d5ced7f40aa77b89740
jon <jon.iturrioz@ehu.es>
2019-12-02 11:47:28 +0100

    Card updated

--- src/main/java/edu/ncsu/monopoly/Card.java
+++ src/main/java/edu/ncsu/monopoly/Card.java
@@ -2,7 +2,7 @@ package edu.ncsu.monopoly;
 
 public abstract class Card {
 
-    public static final int TYPE_CHANCE = 1;
+    public static final int TYPE_CHANCE = 1; 
     public static final int TYPE_CC = 2;
 
     public abstract String getLabel();
